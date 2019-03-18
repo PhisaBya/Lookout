5558bd3a49205ace4390c4c7c5a6904abd50d10e
Phisa Bya <pbya@uncc.edu>
2019-03-18 20:45:15 +0000

    implemented the schedule controller

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,43 @@
 class SchedulesController < ApplicationController
+    def new
+        @schedule = Schedule.new
+    end
+    def show
+        @schedule = Schedule.find(params[:id])
+    end 
+    def create
+        @schedule = Schedule.new(schedule_params)
+        byebug
+        if @schedule.save
+            byebug
+            redirect_to @schedule
+        else
+        render 'new'
+        end
+    end
+    def edit
+        @schedule = Schedule.find(params[:id])
+    end
+    def update
+        @schedule = Schedule.find(params[:id])
+        if @schedule.update(schedule_params)
+            redirect_to @schedule
+        else
+            render 'edit'
+        end
+    end
+    def destroy
+        @schedule = Schedule.find(params[:id])
+        @schedule.destroy
+        redirect_to schedules_path
+    end
+    def index 
+        @schedule = Schedule.all
+    end 
 end
+
+private
+    def schedules_params
+       params.require(:schedule).permit(:title, :text)
+    end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  resources :schedule
+  resources :schedules
   root 'welcome#index'
   
   
