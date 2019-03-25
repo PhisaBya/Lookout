bcfeb15a4afc33495dc132f8460c284a2aaca2a6
Your Name <you@example.com>
2019-03-25 23:58:33 +0000

    linked login page to login button on welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
     def new
     end
+    
+    def login
+        
+    end
+    
+    def create 
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
+  get "users/new" , to: "users#new" , as: "login" 
+  
   resources :schedules
   resources :users
   resources :admins
