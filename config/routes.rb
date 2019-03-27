1508de481265a5b8cc6f0b8cd8cdd4acc6d58615
Your Name <you@example.com>
2019-03-27 21:07:06 +0000

    Fixed css in homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   get 'welcome/index'
   
   get "users/new" , to: "users#new" , as: "login" 
+  get "signup" , to: "signup#index", as: "signup"
   
   resources :schedules
   resources :users
-  resources :admins
-  resources :usertypes
+  resources :signup
   
   root 'welcome#index'
   
