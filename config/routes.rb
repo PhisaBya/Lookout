c757510ca1488e0f4afb8734c6c663b08db2dd9d
Phisa Bya <pbya@uncc.edu>
2019-03-30 21:06:29 +0000

    linked button

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'welcome/index'
   
-  get "users/new" , to: "users#new" , as: "login" 
+  
   
   resources :schedules
   resources :users
   resources :admins
   resources :usertypes
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   
   root 'welcome#index'
   
