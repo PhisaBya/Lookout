96cb65b27aaf7b34b6f2fb438b21f4cebc534dd7
Phisa Bya <pbya@uncc.edu>
2019-04-01 03:21:46 +0000

    Added active storage for image

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  resources :images
   get 'welcome/index'
   
   
   
   resources :schedules
   resources :users
-  resources :admins
-  resources :usertypes
-  resources :users
   resources :sessions, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   
-  root 'welcome#index'
+  root 'images#index'
   
 end
