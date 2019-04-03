05dece0c4e65c1c27ee9bfb43eacdba75bf3a53b
Charlotte Pettit <cpettit4@uncc.edu>
2019-04-03 21:28:22 +0000

    getting all changes made from group

--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,13 @@ Rails.application.routes.draw do
   
   resources :schedules
   resources :users
-<<<<<<< HEAD
   
   root 'welcome#index'
-=======
   resources :sessions, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   #change images to welcome
   root 'welcome#base'
->>>>>>> c662a0d97561432f4f661b7712de65d51c1d850d
   
 end
