a4d683610d8ad868ba544f5ae79494f65c6b19d2
Your Name <you@example.com>
2019-04-03 20:46:00 +0000

    Created dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :images
+  
+  get 'dashboard/index'
   get 'welcome/index'
   
-  
-  
+  resources :images
   resources :schedules
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   
-  root 'images#index'
-  
+ # root 'images#index'
+  root 'welcome#index'
 end
