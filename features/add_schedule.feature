dbfdcdd350c953e4c3013aa38670efb54f885d2c
Charlotte Pettit <cpettit4@uncc.edu>
2019-04-19 02:42:24 +0000

    Added second test scenario

--- features/add_schedule.feature
+++ features/add_schedule.feature
@@ -6,7 +6,8 @@ Feature:
   
 Scenario: As an Admin User I want to be able to add an image of an schedule
   Given I am on the Dashboard
-  When I click on the "Upload Schedule" link
+  When I click on the "Upload schedule" button
   Then I should be on "New Schedules" page
-  When I click on "Create Image" link
+  When I click on "Create Image" button
   Then I should be on the "Show" page
+  
--- features/step_definitions/add_schedule_steps.rb
+++ features/step_definitions/add_schedule_steps.rb
@@ -2,16 +2,16 @@ Given("I am on the Dashboard") do
   visit root_path
 end
 
-When("I click on the {string} link") do |new_image_path|
-  click_link new_image_path
+When("I click on the {string} button") do |page_name|
+  click_button('Upload Schedule')
 end
 
 Then("I should be on {string} page") do |images_path|
-   expect(page).to have_content(images_path)
+  expect(page).to have_content(images_path)
 end
 
-When("I click on {string} link") do |string|
-  click_link 
+When("I click on {string} button") do |string|
+  click_button('Create Image') 
 end
 
 Then("I should be on the {string} page") do |page_name|
