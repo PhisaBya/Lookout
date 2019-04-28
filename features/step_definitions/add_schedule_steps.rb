3229e7daeb7ea8cf2627fe28aebfc901dbaa2319
Your Name <you@example.com>
2019-04-28 06:22:54 +0000

    Added css to homeplage and login/signup pages

--- features/step_definitions/add_schedule_steps.rb
+++ features/step_definitions/add_schedule_steps.rb
@@ -2,13 +2,8 @@ Given("I am on the Dashboard") do
   visit root_path
 end
 
-<<<<<<< HEAD:features/step_definitions/lookout_steps.rb
-When("I click on the {string} link") do |page_name|
-  click_link page_name
-=======
 When("I click on the {string} button") do |page_name|
   click_button('Upload Schedule')
->>>>>>> dbfdcdd350c953e4c3013aa38670efb54f885d2c:features/step_definitions/add_schedule_steps.rb
 end
 
 Then("I should be on {string} page") do |images_path|
